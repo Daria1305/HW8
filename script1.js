905aed9256960d7a8e783bfcf2ff1cb6c394295a
Daria1305 <43953506+Daria1305@users.noreply.github.com>
2019-01-11 21:21:50 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,79 +1,78 @@
 function Machine(power) {
-        this._power = power;
-        this._enabled = false;
+   this._power = power;
+   this._enabled = false;
 
-      var self = this;
+ var self = this;
 
-      this.enable = function() {
-        self._enabled = true;
-      };
+ this.enable = function() {
+   self._enabled = true;
+ };
 
-      this.disable = function() {
-        self._enabled = false;
-      };
-    }
+ this.disable = function() {
+   self._enabled = false;
+ };
+}
 
 
 function Fridge(power) {
-  Machine.apply(this, arguments)
+Machine.apply(this, arguments)
 
-  let food = [];
+let food = []
 
-  this.addFood = function(anything) {
-    if (this._enabled === false) {
-      console.log('Frigo is turned off');
-    }
-    else if (food.length + 1 > power / 100) {
-      console.log('Your frigo is not so powerfull');
-    }
-    else if (this._enalbed === true) {
-      food.push(anything)
-    }
-  }
+this.addFood = function(anything) {
+if (this._enabled === false) {
+ console.log('Frigo is turned off');
+}
+else if (food.length + 1 > power / 100) {
+ console.log('Your frigo is not so powerfull');
+}
+else if (this._enalbed === true) {
+ food.push(anything)
+}
+}
 
-  this.getFood = function() {
-    let allFood = food.slice();
-    return allFood;
-  }
+this.getFood = function() {
+let allFood = food.slice();
+return allFood;
+}
 
 }
 function Sell(power, years_old, condition) {
-  Machine.apply(this, arguments)  //1. Надо ли добавлять эту строку для наследования power ?
-  Fridge.apply(this, arguments)
+Fridge.apply(this, arguments)
 
-  let price = 100
+this.price = 100
 
-  this.addPrice = function(anything){
-    price = price + 10
-  }
+this.addPrice = function(anything){
+price = price + 10
+}
 
-  this.deductPrice = function (anything) {
-    price = price - 10
-  }
+this.reducePrice = function (anything) {
+price = price - 10
+}
 
 
-  if (food.length = 0) {
-    alert("You can sell this fridge")
-    if (power > 1000) {
-      this.addPrice()          //2. Можно ли так вызывать метод?
-    }
-    else if (power < 500) {
-      this.deductPrice()
-    }
-    if (years_old < 5) {
-      this.addPrice()
-    }
-    else if (years_old > 5) {
-      this.deductPrice()
-    }
-    if (condition == 1 && condition == 2 && condition == 3) {
-      this.deductPrice()
-    }
-    else if (condition == 4 && condition == 5) {
-      this.addPrice()
-    }
-  }
-  else if (food.length != 0) {
-    alert("You can not sell this fridge.")
-  }
+if (food.length = 0) {
+alert("You can sell this fridge")
+if (power > 1000) {
+ this.addPrice()
+}
+else if (power < 500) {
+ this.reducePrice()
+}
+if (years_old < 5) {
+ this.addPrice()
+}
+else if (years_old > 5) {
+ this.reducePrice()
+}
+if (condition == 1 && condition == 2 && condition == 3) {
+ this.reducePrice()
+}
+else if (condition == 4 && condition == 5) {
+ this.addPrice()
+}
+}
+else if (food.length != 0) {
+alert("You can not sell this fridge.")
+}
 }
